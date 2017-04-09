b81d323b15fdd95dcd9e601fbfb2887d6e8b2729
claraRigo <clara.rigaud.c3po@gmail.com>
2017-04-09 12:28:07 +0200

    Grammaire au 0904

--- TestParser.py
+++ TestParser.py
@@ -1,57 +1,71 @@
 from pyparsing import *
 
 ### Perfs
-
 GlobalVoicePerf = oneOf('% $ ! !! !!! !N ? ?? ??? ?N !% ?% ?~! !~? ?~~! !~~? @~= =~@ @~~= =~~@ =')
 BarOrnaments = oneOf('| :| |: :|: 1 2 N $ @ >$ <$ <@ >@')
 NoteOrnaments = oneOf('< > << >> + - +- -+ ++ -- =+ =- =+=- =-=+ +=* =-* DGAG DG* =~~++ =~~+ =~~-- =~~- =~~+* =~~-* =~~!! =~~! =~~?? =~~? =~~!* =~~?*')
 ChorsOrnaments = oneOf('-+ +- -~+ +~- -~~+ +~~-')
-
-Info = OneOrMore(Word(alphas))
-InfoLines = Group(Literal('#') + Info)
-VoiceIdentifier = Word(alphas)
-Voice = Word(alphas)
+perfs = GlobalVoicePerf | BarOrnaments | NoteOrnaments | ChorsOrnaments
 
 ### Scale Signature
-
 Octave = oneOf('<< < 0 1 2 3 4 5 6 7 8 9 > >>')
 Pitch = oneOf('A B C D E F G')
 Sign = oneOf('+ -')
 Key = Optional(Sign) + oneOf('A B C D E F G a b c d e f g')
 Fifth = '0' | Sign + oneOf('1 2 3 4 5 6 7')
-Sscale = Key | Fifth
-Lower = oneOf('-N -- --% - -%')
-Raise = oneOf('+N ++ ++% + +%')
-Freq = oneOf('=N =N =NHz')
-Tone = Word(alphas)
-MPN = Word(nums)
-IPN = Group(Pitch + Octave)
-EOS = MPN | IPN
-Step = Group(Tone + (Freq | Raise) + Lower + Raise)
-Cscale = Literal('[') + Step + Literal('$]')
-Ssig = Literal('\$') + EOS + Optional(Group(Literal(':') + (Sscale | Cscale)))
 Degree = Word(alphas)
+Sscale = Key | Fifth
+Lower = OneOrMore("-") + Optional(Optional(Word(nums) | "%"))
+Raise = OneOrMore("+") + Optional(Optional(Word(nums) | "%"))
+Freq = "="+ Word(nums) + Optional(oneOf("' Hz"))
+Tone = Word(alphas)
+MPN = Word(nums) #entre 0 et 127
+IPN = Optional(Sign) + Pitch + Octave
+EOS = MPN | IPN
+Step = Tone + (Freq | Raise) + Lower + Raise + Degree
+Cscale = Literal('[') + Step + Literal('$]')
+
+Ssig = Literal('\$') + EOS + Optional(Literal(':') + (Sscale | Cscale))
 
 ### Beat Signature
 
 Int = Word(nums)
-Pattern = Int | Literal('/')+Int | Literal(' ')+Int
+Pattern = Optional(oneOf(":  ")) + Int 
 SBeat = Word(nums)
 CBeat = Literal('[')+ (Pattern | (Literal('/')+Pattern) | (Literal(' ')+Pattern) ) + Literal(']')
-BPB = Word(nums)
-BPM = Word(nums)
-Bsig = Literal('\%') + BPM + Optional(Group(Literal('~') + BPM)) + Optional(Literal(':') + (SBeat | CBeat))
+BPB = Word(nums) #default 120
+BPM = Word(nums)#default 4
+Bsig = Literal('\%') + BPM + Optional(Literal('~') + BPM) + Optional(Literal(':') + (SBeat | CBeat))
 
 ### Compact Chord Notation
 
-CCN = oneOf('. - + H M m Q X x S s N n L T t A B C D E F G')
+CCN = OneOrMore(oneOf('. - + H M m Q X x S s N n L T t A B C D E F G'))
 
 ### Alterations
+altchars = oneOf('! ? . _ : + - < > ~')
+ALT = OneOrMore(altchars) +  Optional((Word(nums) | oneOf("0 %")))
 
-ALT = oneOf('! ? . _ : + - < > ~')
+#BAR = OneOf((SSIG) + (BSIG)
+BAR = Optional("/")+OneOrMore(Key)+Optional("/")
+BEATS = Optional(";") + Word(alphas) + Optional(";")
 
-File = Literal('O')  + Voice + Ssig + InfoLines
 
+
+Info = OneOrMore(Word(alphas))
+InfoLine = Literal('#') + Group(Info)
+voicename = Word(alphas)
+VoiceIdentifier = ("global"|voicename)
+SplitLine = "|" + OneOrMore(BAR)
+MergeLine = ":" + OneOrMore(BAR)
+phrasetag = "[" + OneOrMore(BAR) + "]"
+groupetag =  "(" + OneOrMore(BEATS) + ")"
+#DataLine = "=" + perfs|phrasetag|groupetag|callphrasetag|callgroupetag
+DataLine = "=" + perfs
+VoiceParamLine = DataLine|SplitLine|MergeLine
+VoiceLine = "O" + VoiceIdentifier + Optional(Ssig) + Optional(Bsig) +Optional(perfs)
+
+VoiceBloc = VoiceLine + Optional(OneOrMore(VoiceParamLine))
+File = Optional(OneOrMore(InfoLine)) + OneOrMore(VoiceBloc)
 aze = "O flute \$C6:C #Voici un exemple"
 
 ddd= File.parseString(aze)
